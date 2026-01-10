7a008fbdb7bbda3cc5e529b010b2331be939c234
Luwify <flyiforlive@gmail.com>
2026-01-10 16:25:24 +0100

    commented out things already defined in arduino_pins.h

--- main/pins_xiao-esp32c6.h
+++ main/pins_xiao-esp32c6.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <stdint.h>
 
+/* already defined in Arduino.h
 static const uint8_t LED_BUILTIN = 15;
 #define BUILTIN_LED LED_BUILTIN  // backward compatibility
 #define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
+
 
 static const uint8_t TX = 16;
 static const uint8_t RX = 17;
@@ -19,6 +21,7 @@ static const uint8_t SCK = 19;
 static const uint8_t A0 = 0;
 static const uint8_t A1 = 1;
 static const uint8_t A2 = 2;
+*/
 
 static const uint8_t D0 = 0;
 static const uint8_t D1 = 1;
